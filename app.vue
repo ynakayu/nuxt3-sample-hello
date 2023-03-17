db6bfe930a7033a2c477e6724416f9d712891b5c
ynakayu <9111754+ynakayu@users.noreply.github.com>
2023-03-17 11:08:12 +0900

    03-08-moustache

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
-    <h1>Hello Nuxt 3!</h1>
+    <h1>{{ message }}</h1>
   </div>
 </template>
+<script>
+  export default {
+    data: function() {
+      return {
+        message: 'Hello World!'
+      }
+    }
+  }
+</script>
